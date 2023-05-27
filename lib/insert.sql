2793e822d180e016e803213f5741d0b22ed49f30
Joab Owala <owalajoab@gmail.com>
2023-05-28 01:02:13 +0300

    Assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', true),
+('Rowdy', 3, 'M', 'Black', 'Energetic', true),
+('Tabitha', 4, 'F', 'Gray', 'Friendly', true),
+('Sergeant Brown', 7, 'M', 'Brown', 'Brave', true),
+('Melissa', 2, 'F', 'Blonde', 'Playful', true),
+('Grinch', 6, 'M', 'Green', 'Mischievous', true),
+('Wendy', 5, 'F', 'White', 'Gentle', true),
+('unnamed', NULL, 'F', NULL, NULL, false);
